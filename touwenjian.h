a4ad38f57e3a4ef97177a2b9477cfcb8b7e37600
qiangang <357499332@qq.com>
2020-04-25 10:35:10 +0800

    touwenjian.h

---
+++ touwenjian.h
@@ -0,0 +1,20 @@
+#pragma once
+#include<stdio.h>
+#include<Windows.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+#include <graphics.h>
+#include <math.h>
+#include <time.h>
+
+//#include <process.h>   
+
+#pragma comment(lib,"Winmm.lib")       // ÒýÓÃ Windows Multimedia API
+
+#define scanf    scanf_s
+#define kbhit    _kbhit
+#define getch    _getch
+//#define _stprintf  _stprintf_s      //VS2015Æ´Ð´³É_swprintf_s 
+#define itoa    _itoa_s
+#define PI 3.14159265359     
